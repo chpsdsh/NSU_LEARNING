2c0673aef20b4b14b9b83b2c40ab990f20e87f7c
И_ПК <bmxsib1@gmail.com>
2023-11-02 18:55:13 +0700

    master

---
+++ Lab3-1/lab3-1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <malloc.h>
+
+
+void swap(int *arr, int ind1, int ind2){
+    int tmp;
+    tmp = arr[ind1];
+    arr[ind1] = arr[ind2];
+    arr[ind2] = tmp;
+}
+
+
+int partition(int *arr, int low, int high) {
+    int pivot = arr[(low + high) / 2];
+    int i = low - 1, j = high + 1;
+    while (1) {
+        do {
+            i++;
+        } while (arr[i] < pivot);
+        do {
+            j--;
+        } while (arr[j] > pivot);
+        if (i >= j) {
+            return j;
+        }
+        swap(arr, i, j);
+    }
+}
+
+
+void quickSort(int *arr, int low, int high) {
+    if (low < high) {
+        int p = partition(arr, low, high);
+        quickSort(arr, low, p);
+        quickSort(arr, p + 1, high);
+    }
+}
+
+int main(){
+    int len;
+
+    scanf_s("%d",&len);
+    int *arr = (int*)malloc(len*sizeof(int));
+    for(int i = 0; i < len; i++)
+        scanf_s("%d",(arr + i));
+
+    quickSort(arr, 0,len-1);
+
+    for(int i = 0; i < len; i++)
+        printf("%d ",arr[i]);
+    free(arr);
+    return 0;
+}
