0afc9ec11e4052640ed3b84ac686718f57902127
unknown <bmxsib1@gmail.com>
2024-02-07 21:41:54 +0700

    add func

--- Lab6-0/avltree.h
+++ Lab6-0/avltree.h
@@ -5,7 +5,14 @@
 #include <stdio.h>
 #include <malloc.h>
 
+int max(int h1, int h2){
+    if (h1>=h2)
+        return h1;
+    else
+        return h2;
+}
 TREE {
+    int key;
     int value;
     int height;
 
@@ -13,29 +20,39 @@ TREE {
 };
 
 
-TREE* create(int value, int height) {
+TREE* create(int key, int value, int height) {
     TREE *res;
     res = (TREE*)malloc(sizeof(TREE));
+    res -> key = key;
     res -> value = value;
-    res -> height = height;
+    res -> height = 0;
     res -> left = NULL;
     res -> right = NULL;
 
     return res;
 }
 
-void add(TREE *tree, int value){
+void add(TREE *tree,int key, int value){
     TREE *child;
     if(tree != NULL){
-        if(tree->left == NULL)
-            child = create(value,tree->height + 1);
-        else if(tree->right == NULL)
-            child = create(value,tree->height + 1);
-    }   else{
-        add(tree->left,value);
-        add(tree->right,value);
+        if(tree->key > key){
+            if(tree->left == NULL)
+                tree->left = create(key,value,0);
+            else
+                add(tree->left,key,value);
+        }
+        else if (tree->key <= key){
+            if(tree->right == NULL)
+                tree->right = create(key,value,0);
+            else
+                add(tree->right,key,value);
+        }
+
     }
 }
 
+void getHeight(TREE *tree){
+   tree->height = max(tree->left->height,tree->right->height) + 1;
+}
 
 #endif //NSU_LEARNING_AVLTREE_H
