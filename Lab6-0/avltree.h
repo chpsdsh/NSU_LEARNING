5c901df8029ab2719327abf5c1ace46c924b2d92
unknown <bmxsib1@gmail.com>
2024-02-11 15:59:32 +0700

    Everything done

--- Lab6-0/avltree.h
+++ Lab6-0/avltree.h
@@ -1,15 +1,19 @@
 #ifndef AVL_TREE_AVL_TREE_H
 #define AVL_TREE_AVL_TREE_H
-#define TREE struct tree
-#include <stdio.h>
-#include <malloc.h>
 
-int max(int h1, int h2){
-    if (h1>=h2)
+#define TREE struct tree
+
+#include <malloc.h>
+#include <stdio.h>
+
+
+int max(int h1, int h2){//returns maximum
+    if (h1 >= h2)
         return h1;
     else
         return h2;
 }
+
 
 TREE{
     int value;
@@ -18,54 +22,64 @@ TREE{
     TREE *right;
 };
 
-TREE*create(int value, TREE*arr, int counter){
+
+TREE*create(int value, TREE*arr, int counter){ //creating new nodes
     arr[counter].height = 0;
     arr[counter].value = value;
     arr[counter].left = arr[counter].right = 0;
     return &arr[counter];
 }
 
-int getHeight(TREE*tree){
+
+int getHeight(TREE*tree){ //returns height
     if (tree == NULL)
         return -1;
     else
         return tree->height;
 }
 
-int updateHeight(TREE *tree){
+
+int updateHeight(TREE *tree){//updates height
     tree->height = max(getHeight(tree->left),getHeight(tree->right)) + 1;
     return tree->height;
 }
 
 
-TREE *rightTurn(TREE*tree){
+TREE *rightTurn(TREE *tree){
     TREE*left = tree->left;
+
     tree->left = left->right;
     left->right = tree;
     tree->height = updateHeight(tree);
     left->height = updateHeight(left);
+
     return left;
 }
 
-TREE *leftTurn(TREE*tree){
+
+TREE *leftTurn(TREE *tree){
     TREE*right = tree->right;
+
     tree->right = right->left;
     right->left = tree;
     tree->height = updateHeight(tree);
-    right->height  = updateHeight(right);
+    right->height = updateHeight(right);
+
     return right;
 }
 
-int IsBalanced(TREE *tree){
+
+int IsBalanced(TREE *tree){//Check for balance
     if(tree != NULL)
         return getHeight(tree->right) - getHeight(tree->left);
     else
         return 0;
 }
 
-TREE*balance(TREE*tree){
+TREE*balance(TREE*tree){//balancing tree
     updateHeight(tree);
     int balance = IsBalanced(tree);
+
     if(balance==-2){
         if(IsBalanced(tree->left) == 1)
             tree->left = leftTurn(tree->left);
@@ -80,7 +94,7 @@ TREE*balance(TREE*tree){
 }
 
 
-TREE*insert(TREE* tree, int value, TREE *nodes){
+TREE*insert(TREE* tree, int value, TREE *nodes){//inserting new nodes to tree
     if(tree == NULL){
         return create( value,nodes,0);
     }
@@ -88,8 +102,8 @@ TREE*insert(TREE* tree, int value, TREE *nodes){
         tree->left = insert(tree->left,value, nodes);
     else
         tree->right = insert(tree->right,value, nodes);
+
     tree->height = updateHeight(tree);
     return balance(tree);
 }
-
 #endif
--- Lab6-0/lab6-0.c
+++ Lab6-0/lab6-0.c
@@ -3,21 +3,27 @@
 #include "avltree.h"
 int main() {
     FILE *file = fopen("in.txt", "r");
+
     int N;
     fscanf(file, "%d", &N);
+
     if(N==0){
         puts("0");
         return 0;
     }
-    TREE*root = NULL;
-    TREE *nodearr = (TREE*)malloc(N*sizeof (TREE));
+
+    TREE *root = NULL;
+    TREE *nodearr = (TREE*)malloc(N*sizeof(TREE));
+
     for(int i = 0; i < N; i++){
         create(0,nodearr, i);
         int value;
         fscanf(file, "%d", &value);
         root = insert(root,value, &nodearr[i]);
     }
-    printf("%d", root->height+1);
+
+    printf("%d", root->height + 1);
+
     fclose(file);
     free(nodearr);
 }
