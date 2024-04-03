8032e1b69d09be3adda127d8d3b23bb329b88c36
chpsdsh <bmxsib1@gmail.com>
2024-04-03 12:08:22 +0700

    kosaraju done

---
+++ kosaraju/stack.c
@@ -0,0 +1,5 @@
+//
+// Created by И-ПК on 03.04.2024.
+//
+
+#include "stack.h"
---
+++ kosaraju/stack.h
@@ -0,0 +1,8 @@
+//
+// Created by И-ПК on 03.04.2024.
+//
+
+#ifndef NSU_LEARNING_STACK_H
+#define NSU_LEARNING_STACK_H
+
+#endif //NSU_LEARNING_STACK_H
