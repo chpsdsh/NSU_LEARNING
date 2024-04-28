e105a4634c8a80743e2aff262cac450a39e25f7b
Ilya Kuznetsov <144603342+chpsdsh@users.noreply.github.com>
2024-04-28 21:55:11 +0700

    Update main.c
    fakn compression

--- huffman/main.c
+++ huffman/main.c
@@ -4,10 +4,11 @@
 #include <locale.h>
 
 #define BUF_SIZE 8
+#define MAX_OCT_SIZE 32
 
 typedef struct node {
-    unsigned long symbol;
-    long int freq;
+    wchar_t symbol;
+    int freq;
     struct node *left;
     struct node *right;
 } NODE;
@@ -18,21 +19,24 @@ typedef struct pqueue {
 } PRIORITY_QUEUE;
 
 typedef struct huffmanCode {
-    unsigned int symbol;
-    unsigned int code;
+    wchar_t symbol;
+    long code;
+    int length;
 } HUFFMANCODE;
 
 typedef struct BitStream {
     FILE *file;
-    unsigned long data;
+    long data;
     int pos;
 } BITSTREAM;
 
 BITSTREAM *createBitStream(FILE *file) {
     BITSTREAM *stream = malloc(sizeof(BITSTREAM));
-    stream->file = file;
-    stream->data = 0;
-    stream->pos = 0;
+    if (stream != NULL) {
+        stream->file = file;
+        stream->data = 0;
+        stream->pos = 0;
+    }
     return stream;
 }
 
@@ -46,36 +50,49 @@ void writeBit(int bit, BITSTREAM *stream) {
     stream->pos++;
 }
 
-void writeByte(unsigned char byte, BITSTREAM *stream) {
-    for (int i = BUF_SIZE; i >= 0; i++) {
-        int bit = (byte >> i) & 1;
+void writeSymbol(wchar_t symbol, BITSTREAM *stream) {
+    for (int i = MAX_OCT_SIZE - 1; i >= 0; i--) {
+        int bit = (symbol >> i) & 1;
         writeBit(bit, stream);
     }
 }
 
+void clearBitstream(BITSTREAM *stream) {
+    stream->data = stream->data << (BUF_SIZE - stream->pos);
+    fwrite(&(stream->data), sizeof(char), 1, stream->file);
+}
+
 PRIORITY_QUEUE *createQ() {
     PRIORITY_QUEUE *queue = malloc(sizeof(PRIORITY_QUEUE));
-    queue->size = 0;
-    queue->heap = NULL;
+    if (queue != NULL) {
+        queue->size = 0;
+        queue->heap = NULL;
+    }
     return queue;
 }
 
 NODE *createN(wchar_t symbol, int freq) {
     NODE *node = malloc(sizeof(NODE));
-    node->symbol = symbol;
-    node->freq = freq;
-    node->left = NULL;
-    node->right = NULL;
+    if (node != NULL) {
+        node->symbol = symbol;
+        node->freq = freq;
+        node->left = NULL;
+        node->right = NULL;
+    }
     return node;
 }
 
-
 void resize(PRIORITY_QUEUE *queue, int size) {
     queue->heap = realloc(queue->heap, size * sizeof(NODE *));
-    queue->size = size;
+    if (queue->heap != NULL) {
+        queue->size = size;
+    }
 }
 
 void enqueue(PRIORITY_QUEUE *queue, NODE *node) {
+    if (queue->heap == NULL) {
+        queue->size = 0;
+    }
     resize(queue, queue->size + 1);
     int index = queue->size - 1;
     queue->heap[index] = node;
@@ -85,18 +102,17 @@ void enqueue(PRIORITY_QUEUE *queue, NODE *node) {
         queue->heap[index - 1] = temp;
         index--;
     }
-
-}
-
-NODE *delete(PRIORITY_QUEUE *queue) {
-    NODE *node = queue->heap[queue->size - 1];
-    queue->heap = realloc(queue->heap, (--queue->size) * sizeof(NODE *));
-    return node;
 }
 
 PRIORITY_QUEUE *initQueue(FILE *input) {
+    if (input == NULL) {
+        return NULL;
+    }
     wint_t symbol;
     PRIORITY_QUEUE *queue = createQ();
+    if (queue == NULL) {
+        return NULL;
+    }
     while ((symbol = fgetwc(input)) != WEOF) {
         int inQueue = 0;
         for (int i = 0; i < queue->size; i++) {
@@ -116,19 +132,8 @@ PRIORITY_QUEUE *initQueue(FILE *input) {
             enqueue(queue, createN(symbol, 1));
         }
     }
-    /*for (int i = 0; i < queue->size; i++)
-        printf("%lc %d\n", queue->heap[i]->symbol, queue->heap[i]->freq);
-        */
     return queue;
 }
-
-
-int emptyQ(PRIORITY_QUEUE *queue) {
-    if (queue->size == 0)
-        return 1;
-    return 0;
-}
-
 
 void sortQueue(PRIORITY_QUEUE *queue, int index) {
     while (index > 0 && queue->heap[index]->freq > queue->heap[index - 1]->freq) {
@@ -139,72 +144,111 @@ void sortQueue(PRIORITY_QUEUE *queue, int index) {
     }
 }
 
+int lastLeave(NODE *root) {
+    return !root->left;
+}
+
 NODE *createTree(PRIORITY_QUEUE *queue) {
-    //NODE *min = delete(queue);
-    //PRIORITY_QUEUE *NewQueue = createQ();
     int index = queue->size - 1;
     while (index > 0) {
-        //printf("%d %d %d\n", index,queue->heap[index]->freq,queue->heap[index - 1]->freq);
-
         NODE *node = createN(WEOF, queue->heap[index]->freq + queue->heap[index - 1]->freq);
+        if (node == NULL) {
+            return NULL;
+        }
         node->left = queue->heap[index - 1];
         node->right = queue->heap[index];
         queue->heap[--index] = node;
-        //printf("%d\t", queue->heap[index]->freq);
         resize(queue, queue->size - 1);
-        //index--;
-        //printf("%d\n", index);
         sortQueue(queue, index);
-        //printf("%d\n", queue->heap[index]->freq);
-
     }
-    //puts("penis");
-    //InOrder(queue->heap[0]);
     return queue->heap[0];
 }
 
-
-void GetCocks(NODE *root, unsigned int code, HUFFMANCODE *codes, int *index) {
+void GetCocks(NODE *root, unsigned int code, int length, HUFFMANCODE *codes, int *index) {
     if (root->left == NULL) {
         codes[*index].symbol = root->symbol;
-        codes[(*index)++].code = code;
-        // printf("%d %lc\n", *index, codes[(*index) - 1].symbol);
+        codes[*index].length = length;
+        codes[*index].code = code;
+        (*index)++;
         return;
     }
-    GetCocks(root->left, code << 1, codes, index);
-    GetCocks(root->right, (code << 1) | 1, codes, index);
+    GetCocks(root->left, code << 1, length + 1, codes, index);
+    GetCocks(root->right, (code << 1) | 1, length + 1, codes, index);
 }
 
-void treeToFile(HUFFMANCODE *codes, int size) {
-    FILE *output = fopen("out.txt", "w");
-    if (!output) {
+void treeToFile(NODE *root, BITSTREAM *stream) {
+    if (lastLeave(root)) {
+        writeBit(1, stream);
+        writeSymbol(root->symbol, stream);
         return;
     }
-
-    for (int i = 0; i < size; i++)
-        fprintf(output, "%d %d ", codes[i].symbol, codes[i].code);
-    fclose(output);
+    writeBit(0, stream);
+    treeToFile(root->left, stream);
+    treeToFile(root->right, stream);
 }
 
-void GetCocksTable(NODE *root, HUFFMANCODE *codes) {
-    int index = 0;
-    GetCocks(root, 0, codes, &index);
-    treeToFile(codes, index);
+void codeInput(wchar_t symbol, HUFFMANCODE *codes, BITSTREAM *stream, int codesLen) {
+    int i;
+    for (i = 0; i < codesLen; i++) {
+        if (codes[i].symbol == symbol) {
+            break;
+        }
+    }
+    for (int j = codes[i].length - 1; j >= 0; j--) {
+        int bit = (codes[i].code >> j) & 1;
+        writeBit(bit, stream);
+    }
 }
 
-void encode(FILE *input) {
+void encode(FILE *input, FILE *output) {
+    if (input == NULL || output == NULL) {
+        return;
+    }
     PRIORITY_QUEUE *queue = initQueue(input);
-    //BITSTREAM *stream = createBitStream(output);
-    HUFFMANCODE *codes = malloc(queue->size * sizeof(HUFFMANCODE *));
+    if (queue == NULL) {
+        return;
+    }
+    BITSTREAM *stream = createBitStream(output);
+    if (stream == NULL) {
+        free(queue);
+        return;
+    }
+    HUFFMANCODE *codes = malloc(queue->size * sizeof(HUFFMANCODE));
+    if (codes == NULL) {
+        free(queue);
+        free(stream);
+        return;
+    }
     NODE *root = createTree(queue);
-    GetCocksTable(root, codes);
+    if (root == NULL) {
+        free(queue);
+        free(stream);
+        free(codes);
+        return;
+    }
+    int index = 0;
+    GetCocks(root, 0, 0, codes, &index);
+    fwrite(&(root->freq), sizeof(int), 1, stream->file);
+    treeToFile(root, stream);
+    rewind(input);
+    wint_t symbol;
+    while ((symbol = fgetwc(input)) != WEOF) {
+        codeInput(symbol, codes, stream, index);
+    }
+    clearBitstream(stream);
+    free(queue);
     free(codes);
+    free(stream);
 }
 
 int main() {
     setlocale(LC_ALL, "");
     FILE *input = fopen("in.txt", "r, ccs=UTF-8");
-    encode(input);
-    fclose(input);
+    FILE *output = fopen("out.txt", "w+");
+    if (input != NULL && output != NULL) {
+        encode(input, output);
+        fclose(input);
+        fclose(output);
+    }
     return 0;
 }
